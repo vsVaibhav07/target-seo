065a93671b2c378d352debc0036987d37621787b
vsVaibhav07 <vaibhavshuklamdi@gmail.com>
2026-01-24 01:13:30 +0530

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // Ye line icons ko automatically optimize kar degi
+    optimizePackageImports: ['lucide-react', 'framer-motion'],
+  },
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;
